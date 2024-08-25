ed3e239d03c28d55675e17a5186cc7ab96617a4f
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-08-26 01:17:34 +0300

    Update KinneretParkingLot.h

--- Arduino/KinneretParkingLot/KinneretParkingLot.h
+++ Arduino/KinneretParkingLot/KinneretParkingLot.h
@@ -1,1 +1,31 @@
+#ifndef PARKINGCLIENT_H
+#define PARKINGCLIENT_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ArduinoWebsockets.h>
+
+using namespace websockets;
+
+class MyLotNode {
+private:
+    WebsocketsClient wsClient;
+	const char* SSID = "inanforever";
+	const char* PSWD = "0509232623";
+	const char* websocket_server = "192.168.1.10";
+	const uint16_t websocket_port = 5555;
+	IPAddress gateway = IPAddress(192, 168, 1, 1);    // Router's IP address (has to be verified for each network but certain conventions are common)
+	IPAddress subnet = IPAddress(255, 255, 255, 0);   // Subnet mask for the local network
+    IPAddress local_IP;  // Declared to be further passed into constructor
+
+
+public:
+    MyLotNode(const IPAddress& myIP);
+    void networkSetup();
+    void connectWebSocket();
+	uint16_t getDistance(int myTrig, int myEcho);
+	void sendDistance(String myString, int myThreshold, uint16_t myDistance);
+};
+
+#endif
 
