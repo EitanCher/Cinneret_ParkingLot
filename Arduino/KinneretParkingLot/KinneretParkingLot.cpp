473de6b2b93bcaff4fb59475ea0f14bd39560abc
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-08-31 15:43:23 +0300

    Update KinneretParkingLot.cpp
    Working version for the following functionality using custom library:
    - connecting to WIFI network, having static IP;
    - connecting to the Websocket server;
    - reading a distance and sending it to the server if under threshold

--- Arduino/KinneretParkingLot/KinneretParkingLot.cpp
+++ Arduino/KinneretParkingLot/KinneretParkingLot.cpp
@@ -1,6 +1,7 @@
 #include "KinneretParkingLot.h"
 
-MyLotNode::MyLotNode(const IPAddress& myIP) : local_IP(myIP) {}
+MyLotNode::MyLotNode(const IPAddress& myIP) 
+	: local_IP(myIP) {}
     // Syntax memo:
 	// the constructor accepts an IPAddress object by reference, which avoids copying the object. 
 	// ': local_IP(myIP)' is a member initializer list, used to initialize member variables of a class before the constructor body runs
@@ -12,27 +13,29 @@ void MyLotNode::networkSetup() {
 	// Configure static IP for the current Sensor Node:
 	if (!WiFi.config(local_IP, this->gateway, this->subnet)) Serial.println("STA Failed to configure");
 
-	WiFi.begin(this->SSID, this->PSWD);
+	wifiMulti.addAP(this->SSID, this->PSWD); // Preferred over 'WiFi.begin(this->SSID, this->PSWD)' due to support for using static IP 
 
-	while (WiFi.status() != WL_CONNECTED) {
+	while(wifiMulti.run() != WL_CONNECTED) {
 		Serial.println("Attempting to connect to network...");
 		delay(100);
 	}
 	Serial.println("Connected to network");
-	Serial.print("Local IP: ");
-	Serial.println(local_IP);
+	Serial.print("Local AP IP: ");
+	Serial.println(WiFi.localIP());
 }
 
 void MyLotNode::connectWebSocket() {
-	while(!this->wsClient.connect(this->websocket_server, this->websocket_port, "/")) {
+	while(!wsClient.connect(this->websocket_server, this->websocket_port, "/")) {
 		delay(100);
+		Serial.println(this->websocket_port);
+		Serial.println(this->websocket_server);
 		Serial.println(".... Attempting to connect to websocket server ....");
 	}
 	Serial.println("Websocket server connected");
 }
 
-uint16_t MyLotNode::getDistance(int myTrig, int myEcho) {
-  uint16_t duration, distance;
+void MyLotNode::readDistance(int myTrig, int myEcho) {
+  uint16_t duration;
 
   // Trigger the HC-SR04 to send an ultrasonic pulse:
   digitalWrite(myTrig, LOW);
@@ -45,23 +48,22 @@ uint16_t MyLotNode::getDistance(int myTrig, int myEcho) {
   duration = pulseIn(myEcho, HIGH); //Returns the length of the pulse in microseconds or 0 if no complete pulse was received within the timeout
 
   // Calculate the distance in centimeters
-  distance = duration * 0.034 / 2;	// Sound speed is about 34mps
+  this->distance = duration * 0.034 / 2;	// Sound speed is about 34mps
   Serial.print("Distance: ");
-  Serial.print(distance);
+  Serial.print(this->distance);
   Serial.println(" cm");
-  
-  return distance;
 }
 
-void MyLotNode::sendDistance(String myString, int myThreshold, uint16_t myDistance) {
-  // Passing 'myClient' by pointer allows NULL values
+void MyLotNode::sendDistance(String myString, int myThreshold, int myTrig, int myEcho) {  
+  this->readDistance(myTrig, myEcho);
+  
   String msg_event = myString + "_event";
   String msg_zero = myString + "_0_distance";
-  if (myDistance > 0 && myDistance < myThreshold) {
+  if (this->distance > 0 && this->distance < myThreshold) {
     Serial.print("Object detected on ");
     Serial.println(myString);
-    this->wsClient.send(msg_event);
+    wsClient.send(msg_event);
   }
-  else if (myDistance == 0) this->wsClient.send(msg_zero);
-  //else this->wsClient.send("no_object");
+  else if (this->distance == 0) wsClient.send(msg_zero);
+  //else wsClient.send("no_object");
 }
