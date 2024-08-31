a05a68d28a2dc84abf8955bda592bd384d0818d3
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-08-31 15:48:11 +0300

    Update KinneretParkingLot.h
    Working version for the following functionality using custom library:
    - connecting to WIFI network, having static IP;
    - connecting to the Websocket server;
    - reading a distance and sending it to the server if under threshold

--- Arduino/KinneretParkingLot/KinneretParkingLot.h
+++ Arduino/KinneretParkingLot/KinneretParkingLot.h
@@ -3,29 +3,34 @@
 
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiMulti.h>
 #include <ArduinoWebsockets.h>
 
 using namespace websockets;
 
 class MyLotNode {
 private:
-    WebsocketsClient wsClient;
 	const char* SSID = "inanforever";
 	const char* PSWD = "0509232623";
 	const char* websocket_server = "192.168.1.10";
 	const uint16_t websocket_port = 5555;
+	uint16_t distance;
 	IPAddress gateway = IPAddress(192, 168, 1, 1);    // Router's IP address (has to be verified for each network but certain conventions are common)
 	IPAddress subnet = IPAddress(255, 255, 255, 0);   // Subnet mask for the local network
-    IPAddress local_IP;  // Declared to be further passed into constructor
+
+	WiFiMulti wifiMulti;
+    	WebsocketsClient wsClient;
+
+    // Declared to be further passed into constructor:
+	IPAddress local_IP;
 
 
 public:
-    MyLotNode(const IPAddress& myIP);
-    void networkSetup();
-    void connectWebSocket();
-	uint16_t getDistance(int myTrig, int myEcho);
-	void sendDistance(String myString, int myThreshold, uint16_t myDistance);
+	MyLotNode(const IPAddress& myIP);
+	void networkSetup();
+	void connectWebSocket();
+	void readDistance(int myTrig, int myEcho);
+	void sendDistance(String myString, int myThreshold, int myTrig, int myEcho);
 };
 
 #endif
-
