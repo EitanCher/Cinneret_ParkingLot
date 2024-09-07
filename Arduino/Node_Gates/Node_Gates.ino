e5fb65b96bd30ef987f0441a07d1fdc4eee9d3da
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-09-08 00:27:20 +0300

    Update Node_Gates.ino
    Support running a rollcall with predetermined interval

--- Arduino/Node_Gates/Node_Gates.ino
+++ Arduino/Node_Gates/Node_Gates.ino
@@ -3,8 +3,9 @@
 #include <ArduinoWebsockets.h>
 #include <KinneretParkingLot.h>
 
+// === Pins Definitions: =====================================================
 // A switch between Entrance / Exit mode of the Gate. Set to enable using one board for both cases due to HW restrictions of the project
-#define GATE_SWITCH 2
+#define SWITCH_ENTR 2
 #define SWITCH_EXIT 4
 // HC-SR04 (ultrasonic sensor) pins:
 #define PIN_TRIG_ENTR 19
@@ -15,46 +16,71 @@
 #define THRESHOLD_GATE 20
 #define THRESHOLD_SLOT 20
 
-//Create an object for Client:
-IPAddress local_IP(192, 168, 1, 200); // Manually set for current device (Ultrasonic Sensor on Entry). Has to be verified not to conflict with other devices.
-MyLotNode myClient(local_IP);
+unsigned long myTimer;
+
+// Local IPs are manually set for current devices (Ultrasonic Sensors on Entry/Exist). 
+// The IPs have to be verified not to conflict with other devices.
+// The IPs have to match the relevant data in the DB
+IPAddress local_IP_entr(192, 168, 1, 2); 
+IPAddress local_IP_exit(192, 168, 1, 3);
+
+// === Create objects for Clients: ==========================================
+MyLotNode myClient_Entry(local_IP_entr);
+MyLotNode myClient_Exit(local_IP_exit);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting....................");
   
   // Initialize the pins:
-  pinMode(GATE_SWITCH, INPUT_PULLUP);
+  pinMode(SWITCH_ENTR, INPUT_PULLUP);
   pinMode(SWITCH_EXIT, INPUT_PULLUP);
   pinMode(PIN_TRIG_ENTR, OUTPUT);
   pinMode(PIN_ECHO_ENTR, INPUT);
   pinMode(PIN_TRIG_EXIT, OUTPUT);
   pinMode(PIN_ECHO_EXIT, INPUT);
 
-  //Initiate the WIFI connection:
-  myClient.networkSetup();
+  // Initialize WIFI connection:
+  myClient_Entry.networkSetup();
+  myClient_Exit.networkSetup();
 
   // Connect to the server:
-  myClient.connectWebSocket();  
+  myClient_Entry.defineWSClient();  
+  myClient_Exit.defineWSClient();  
+
+  myTimer = millis();
 }
 
 void loop() {
-  Serial.println("this is loop");
+  // Perform rollcall for the clients:
+  if (millis() - myTimer >= myClient_Entry.getInterval()) {
+    myClient_Entry.rollcall();  
+    myTimer = millis();  // Reset timer
+  }
+  if (millis() - myTimer >= myClient_Exit.getInterval()) {
+    myClient_Exit.rollcall();  
+    myTimer = millis();  // Reset timer
+  }
 
-  // Read the state of the switch
-  int gateState = digitalRead(GATE_SWITCH);
+  // Read the state of the switches:
+  int StateEntr = digitalRead(SWITCH_ENTR);
+  int StateExit = digitalRead(SWITCH_EXIT);
+  bool isEntr, isExit;
   
   // Check the mode of the Gate:
-  if (gateState == LOW) { // GATE_SWITCH pin is connected to ground
+  if (StateEntr == LOW && StateExit == HIGH)  // SWITCH_ENTR pin is connected to ground, SWITCH_EXIT pin unconnected
+    {isEntr = true; isExit = false;}
+  else   
+    {isExit = true; isEntr = false;}
+
+  // Read the ultrasonic on the active gate and send to server if an object detected:
+  if (isEntr) {
     Serial.println("Acting as Entry Gate -------------------------------");
-    // Read the ultrasonic on Entry gate and send to server if an object detected:
-    myClient.sendDistance("entry", THRESHOLD_GATE, PIN_TRIG_ENTR, PIN_ECHO_ENTR);
+    myClient_Entry.sendDistance("entry", THRESHOLD_GATE, PIN_TRIG_ENTR, PIN_ECHO_ENTR);
   } else {
     Serial.println("Acting as Exit Gate -------------------------------");
-    // Read the ultrasonic on Exit gate and send to server if an object detected:
-    myClient.sendDistance("exit", THRESHOLD_GATE, PIN_TRIG_EXIT, PIN_ECHO_EXIT);
+    myClient_Exit.sendDistance("exit", THRESHOLD_GATE, PIN_TRIG_EXIT, PIN_ECHO_EXIT);
   }
   
   delay(2000);
 }
-
