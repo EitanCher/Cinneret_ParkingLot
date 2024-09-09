2a1ba93caf3e67c27349d765b9b4dea907c4e309
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-09-09 23:57:57 +0300

    Update KinneretParkingLot.h
    Successfully receive messages and events from the server

--- Arduino/KinneretParkingLot/KinneretParkingLot.h
+++ Arduino/KinneretParkingLot/KinneretParkingLot.h
@@ -15,30 +15,30 @@ private:
 	const char* websocket_server = "192.168.1.10";
 	const uint16_t websocket_port = 5555;
 	uint16_t distance;
-	const int rollcallInterval = 600000;
+	const int rollcallInterval = 60000;
 	bool takePicture = false;
 	IPAddress gateway = IPAddress(192, 168, 1, 1);    // Router's IP address (has to be verified for each network but certain conventions are common)
 	IPAddress subnet = IPAddress(255, 255, 252, 0);   // Subnet mask for the local network
 
 	WiFiMulti wifiMulti;
-    WebsocketsClient wsClient;
+	WebsocketsClient wsClient;
 
     // Declared to support the constructor syntax:
 	IPAddress local_IP;
 
 
 public:
-    MyLotNode(const IPAddress& myIP);
-    void networkSetup();
-	void onMessageCallback(WebsocketsMessage input);
-    void defineWSClient();
+	MyLotNode(const IPAddress& myIP);
+	void networkSetup();
+	void defineWSClient();
+	void onMessageCallback(WebsocketsMessage message);
 	void rollcall();
 	void readDistance(int myTrig, int myEcho);
 	void sendDistance(String myString, int myThreshold, int myTrig, int myEcho);
-    int getInterval() const { return rollcallInterval; }
+	int getInterval() const { return rollcallInterval; }
 	bool isShotRequired() { return takePicture; }
-//	bool isShotRequired() const { return takePicture; }
 	void setShotRequire(bool b) { takePicture = b; }
+	void handle() { wsClient.poll(); }
 };
 
 #endif
