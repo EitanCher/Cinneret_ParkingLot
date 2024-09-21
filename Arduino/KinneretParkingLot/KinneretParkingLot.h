540ec1ca0f6b925f807dab8fecd9fc3459fb6f52
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-09-22 01:03:54 +0300

    Support Servo open

--- Arduino/KinneretParkingLot/KinneretParkingLot.h
+++ Arduino/KinneretParkingLot/KinneretParkingLot.h
@@ -13,13 +13,16 @@ private:
 	const char* SSID = "inanforever";
 	const char* PSWD = "0509232623";
 	const char* websocket_server = "192.168.1.10";
+	//const char* websocket_server = "172.21.160.1";
 	const uint16_t websocket_port = 5555;
 	const unsigned long connectionTimeout = 300000; // Set timeout to 5 minutes
 	unsigned long connectionStartTime = 0;
 	uint16_t distance;
 	const unsigned long rollcallInterval = 60000;
 	unsigned long timer = 0;
-	bool takePicture = false;
+	bool flag_openGate = false;
+	bool flag_takePicture = false;
+	bool isGateOpen = false;
 	bool isConnectionSuccess = true;
 	IPAddress gateway = IPAddress(192, 168, 1, 1);    // Router's IP address (has to be verified for each network but certain conventions are common)
 	IPAddress subnet = IPAddress(255, 255, 252, 0);   // Subnet mask for the local network
@@ -38,14 +41,17 @@ public:
 	int 	getInterval() const { return rollcallInterval; }
 	void 	handle();
 	bool	isConnectionSucceed() { return isConnectionSuccess; }
-	bool 	isShotRequired() { return takePicture; }
+	bool 	isOpenRequired() { return flag_openGate; }
+	bool 	isShotRequired() { return flag_takePicture; }
 	void 	networkSetup();
 	void 	onMessageCallback(WebsocketsMessage message);
 	void 	readDistance(int myTrig, int myEcho);
 	void 	rollcall();
 	void 	sendDistance(String myString, int myThreshold, int myTrig, int myEcho);
 	void 	sendPicture(const char* data, const size_t len);
-	void 	setShotRequire(bool b) { takePicture = b; }
+	void 	setGateStatus(bool b) { isGateOpen = b; }
+	void 	setOpenRequest(bool b) { flag_openGate = b; }
+	void 	setShotRequire(bool b) { flag_takePicture = b; }
 };
 
 #endif
