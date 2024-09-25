c3d62f75aab6bfc7225bc301e620624cce2e6061
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-09-26 01:21:03 +0300

    Polymorphism approach implemented

--- Arduino/KinneretParkingLot/KinneretParkingLot.h
+++ Arduino/KinneretParkingLot/KinneretParkingLot.h
@@ -9,50 +9,40 @@
 using namespace websockets;
 
 class MyLotNode {
-private:
-	const char* SSID = "inanforever";
-	const char* PSWD = "0509232623";
-	const char* websocket_server = "192.168.1.10";
-	//const char* websocket_server = "172.21.160.1";
-	const uint16_t websocket_port = 5555;
-	const unsigned long connectionTimeout = 300000; // Set timeout to 5 minutes
-	unsigned long connectionStartTime = 0;
-	uint16_t distance;
-	const unsigned long rollcallInterval = 60000;
-	unsigned long timer = 0;
-	bool block_usonic = false;
-	bool flag_gateClose = false;
-	bool flag_gateOpen = false;
-	bool flag_takePicture = false;
-	bool isConnectionSuccess = true;
-	IPAddress gateway = IPAddress(192, 168, 1, 1);    // Router's IP address (has to be verified for each network but certain conventions are common)
-	IPAddress subnet = IPAddress(255, 255, 252, 0);   // Subnet mask for the local network
+protected:
+	const char* 	SSID = "inanforever";
+	const char* 	PSWD = "0509232623";
+	const char* 	websocket_server = "192.168.1.10";
+	const uint16_t	websocket_port = 5555;
+	const uint32_t	connectionTimeout = 300000; // Set timeout to 5 minutes
+	unsigned long 	connectionStartTime = 0;
+	const uint32_t	rollcallInterval = 60000;
+	unsigned long 	timer = 0;
+	bool 		isConnectionSuccess = true;
+	IPAddress 	gateway =	IPAddress(192, 168, 1, 1);    // Router's IP address (has to be verified for each network but certain conventions are common)
+	IPAddress 	subnet =	IPAddress(255, 255, 252, 0);   // Subnet mask for the local network
 
 	WiFiMulti wifiMulti;
 	WebsocketsClient wsClient;
 
-    // Declared to support the constructor syntax:
+    	// Declared to support the constructor syntax:
 	IPAddress local_IP;
 
 
 public:
 	MyLotNode(const IPAddress& myIP);
+	virtual void onMessageCallback(WebsocketsMessage message) = 0;
+		// Syntax memo: 
+		//The above is Pure Virtual function.
+		//It makes class MyLotNode to be abstract.
+		//Every child class has to implement this function even if empty.
 	void 	connectToServer();
 	void 	defineWSClient();
 	int 	getInterval() const { return rollcallInterval; }
 	void 	handle();
 	bool	isConnectionSucceed() { return isConnectionSuccess; }
-	bool 	isCloseRequired() { return flag_gateClose; }
-	bool 	isOpenRequired() { return flag_gateOpen; }
-	bool 	isShotRequired() { return flag_takePicture; }
 	void 	networkSetup();
-	void 	onMessageCallback(WebsocketsMessage message);
-	void 	readDistance(int myTrig, int myEcho);
 	void 	rollcall();
-	void 	sendDistance(String myString, int myThreshold, int myTrig, int myEcho);
-	void 	sendPicture(const char* data, const size_t len);
-	void 	setCloseRequest(bool b) { flag_gateClose = b; }
-	void 	setShotRequire(bool b) { flag_takePicture = b; }
 };
 
 #endif
