e8347fe2767d8e8a50319adc35e2d600a61c38a0
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-09-26 01:27:16 +0300

    Polymorphism approach implemented

---
+++ Arduino/KinneretParkingLot/KinneretParkingLot_Camera.h
@@ -0,0 +1,19 @@
+#ifndef PARKINGCAMERA
+#define PARKINGCAMERA
+
+#include "KinneretParkingLot.h"
+
+class Camera : public MyLotNode {
+	
+private:
+	bool flag_takePicture = false;
+
+public:
+	Camera(const IPAddress& myIP);
+	bool 	isShotRequired() { return flag_takePicture; }
+	void 	onMessageCallback(WebsocketsMessage message) override;
+	void 	sendPicture(const char* data, const size_t len);
+	void 	setShotRequire(bool b) { flag_takePicture = b; }
+};
+
+#endif
