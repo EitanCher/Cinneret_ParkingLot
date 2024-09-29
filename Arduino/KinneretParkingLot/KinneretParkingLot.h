42ba2098ca5c1459bbaa30207fc80f10a0c66f45
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-09-30 00:39:00 +0300

    Class hierarchy optimized

--- Arduino/KinneretParkingLot/KinneretParkingLot.h
+++ Arduino/KinneretParkingLot/KinneretParkingLot.h
@@ -10,16 +10,17 @@ using namespace websockets;
 
 class MyLotNode {
 protected:
-	const char* 	SSID = "*********";
-	const char* 	PSWD = "*********";
-	const char* 	websocket_server = "192.168.110.3";
+	const char* 	SSID = "...";
+	const char* 	PSWD = "...";
+	const char* 	websocket_server = "192.168.1.10";
+//	const char* 	websocket_server = "192.168.110.3";
 	const uint16_t	websocket_port = 5555;
 	const uint32_t	connectionTimeout = 300000; // Set timeout to 5 minutes
 	unsigned long 	connectionStartTime = 0;
 	const uint32_t	rollcallInterval = 60000;
 	unsigned long 	timer = 0;
 	bool			isConnectionSuccess = true;
-	IPAddress		gateway =	IPAddress(192, 168, 110, 1);    // Router's IP address (has to be verified for each network but certain conventions are common)
+	IPAddress		gateway =	IPAddress(192, 168, 1, 1);    // Router's IP address (has to be verified for each network but certain conventions are common)
 	IPAddress		subnet =	IPAddress(255, 255, 252, 0);   // Subnet mask for the local network
 
 	WiFiMulti wifiMulti;
