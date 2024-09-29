3582b63c40db0b38261f885ea2b46176602d9890
Eitan Cherkasov <102179966+EitanCher@users.noreply.github.com>
2024-09-30 00:40:47 +0300

    Class hierarchy optimized

--- Arduino/KinneretParkingLot/KinneretParkingLot_Gate.h
+++ Arduino/KinneretParkingLot/KinneretParkingLot_Gate.h
@@ -1,23 +1,21 @@
 #ifndef PARKINGGATE
 #define PARKINGGATE
 
-#include "KinneretParkingLot.h"
+#include "KinneretParkingLot_Sensor.h"
 
-class Gate : public MyLotNode {
+class ParkingGate : public ParkingSensor {
 	
-protected:
-	bool 	 block_usonic = false;
-	bool 	 flag_gateClose = false;
-	bool 	 flag_gateOpen = false;
+private:
+	bool flag_gateClose = false;
+	bool flag_gateOpen = false;
 
 public:
-	Gate(const IPAddress& myIP);
-	virtual void checkDistance(String myString, int myThreshold, int myTrig, int myEcho);
-	bool 		isCloseRequired() { return flag_gateClose; }
-	bool 		isOpenRequired() { return flag_gateOpen; }
-	void 		onMessageCallback(WebsocketsMessage message) override;
-	uint16_t	readDistance(int myTrig, int myEcho);
-	void 		setCloseRequest(bool b) { flag_gateClose = b; }
+	ParkingGate(const IPAddress& myIP);
+	void checkDistance(String myString, int myThreshold, int myTrig, int myEcho) override;
+	bool isCloseRequired() { return flag_gateClose; }
+	bool isOpenRequired() { return flag_gateOpen; }
+	void onMessageCallback(WebsocketsMessage message) override;
+	void setCloseRequest(bool b) { flag_gateClose = b; }
 };
 
 #endif
